630144fa4d1af71e3ec8a1ba474fd1d36596cbee
izica <artemiztomska@gmail.com>
2019-04-07 14:46:41 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# phalcon-validation
+# Phalcon validation
+
 Handy interface for Phalcon\Validation
+
+### Installation
+```
+composer require izica/phalcon-validation
+```
+
+### Api
+Izica\Validation
+* __construct($arOptions)  // example below
+* validate($arData)
+* static required
+* static numeric
+* static email
+* TODO ----
+* length
+* callback
+* etc
+
+### Usage
+```php
+use Izica\Validation;
+use Phalcon\Mvc\Controller;
+
+class ExampleController extends Controller {
+    public function indexAction() {
+        $validation = new Validation([
+            'email' => [Validation::required(), Validation::email()],
+            'num' => [Validation::required(), Validation::numeric()],
+        ]);
+        $arMessages = $validation->validate($_POST);
+        if ($arMessages) {
+            // validation error
+        }
+        // validation success
+    }
+}
+```
+
+```
+$arMessages = $validation->validate($_POST);
+
+Request
+email: qwe
+num: 
+
+Response: 
+Array
+(
+    [0] => Array
+        (
+            [type] => email
+            [message] => email is not valid
+            [field] => email
+        )
+
+    [1] => Array
+        (
+            [type] => num
+            [message] => num is required
+            [field] => num
+        )
+
+    [2] => Array
+        (
+            [type] => num
+            [message] => num is not numeric
+            [field] => num
+        )
+)
+
+```
+
---
+++ Validation.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Izica;
+
+use Phalcon\Validation as PhalconValidation;
+use Phalcon\Validation\Validator\Email;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\Numericality;
+
+class Validation {
+    private static $sTypeRequired = 'required';
+    private static $sTypeNumeric = 'numeric';
+    private static $sTypeEmail = 'email';
+    private $arOptions = [];
+    public static $arValidators = [
+        'required' => PresenceOf::class,
+        'numeric' => Numericality::class,
+        'email' => Email::class,
+    ];
+
+    function __construct($arOptions) {
+        $this->arOptions = $arOptions;
+    }
+
+    private static function getValidator($sName, $arOptions) {
+        if (self::$arValidators[$sName]) {
+            return new self::$arValidators[$sName]($arOptions);
+        }
+        return false;
+    }
+
+    public function validate($arData, $bKeys = true) {
+        $obValidation = new PhalconValidation();
+        foreach ($this->arOptions as $sField => $arFieldOptions) {
+            foreach ($arFieldOptions as $arFieldOption) {
+                $obValidator = self::getValidator($arFieldOption['type'], $arFieldOption['options']);
+                if ($obValidator !== false) {
+                    $obValidation->add($sField, $obValidator);
+                }
+            }
+        }
+        $obMessages = $obValidation->validate($arData);
+        $arMessages = [];
+        if ($bKeys) {
+            foreach ($obMessages as $obMessage) {
+                $arMessages[$obMessage->getField()][$obMessage->getType()] = $obMessage->getMessage();
+            }
+        }else{
+            foreach ($obMessages as $obMessage) {
+                $arMessages[] = $obMessage->getMessage();
+            }
+        }
+        return $arMessages;
+    }
+
+    public static function required(
+        $arOptions = [
+            'message' => ':field is required'
+        ]
+    ) {
+        return [
+            'type' => self::$sTypeRequired,
+            'options' => $arOptions
+        ];
+    }
+
+    public static function numeric(
+        $arOptions = [
+            'message' => ':field is not numeric'
+        ]
+    ) {
+        return [
+            'type' => self::$sTypeNumeric,
+            'options' => $arOptions
+        ];
+    }
+
+    public static function email(
+        $arOptions = [
+            'message' => ':field is not valid'
+        ]
+    ) {
+        return [
+            'type' => self::$sTypeEmail,
+            'options' => $arOptions
+        ];
+    }
+}
